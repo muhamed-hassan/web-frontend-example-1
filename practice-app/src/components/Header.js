e067d16329c11d3ae14dfdc91d897b1fda6bdca8
muhamed-hassan <mohamed.hassan.work@gmail.com>
2026-01-02 08:01:18 +0200

    remove website's logo

--- practice-app/src/components/Header.js
+++ practice-app/src/components/Header.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Logo from '../assets/logo.jpg';
 
 export default class Header extends React.Component {
 
     render() {
         return (
-            <header>
-                <img class="logo" src={Logo} alt="Logo" />
-            </header>
+            <header></header>
         );
     }
 
